449db4fea973fc95624d753d8878b1c63be5c4cb
stiratto <moranmoralesj4@gmail.com>
2025-06-08 11:14:29 -0500

    created usercontext

---
+++ src/context/userContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useEffect, useState } from "react"
+
+interface User {
+   token: string,
+   role: string,
+   username: string
+}
+
+interface UserContextType {
+   user: User,
+   setUser: Dispatch<SetStateAction<User>>
+}
+
+const UserContext = createContext<UserContextType | null>(null)
+
+
+export const UserProvider = ({children}: {children: ReactNode}) => {
+   const [user, setUser] = useState<User>(() => {
+      const storedUser = localStorage.getItem("user");
+      return storedUser ? JSON.parse(storedUser) : {} as User;
+   })
+
+   useEffect(() => {
+      localStorage.setItem("user", JSON.stringify(user))
+   }, [user])
+
+   return (
+      <UserContext.Provider value={{user, setUser}}>
+         {children}
+      </UserContext.Provider>
+   )
+}
+
+export const useUserContext = () => {
+   const context = useContext(UserContext)
+   if (!context) {
+      throw new Error("useUserContext() must be used within a UserContext")
+   }
+   return context 
+}
