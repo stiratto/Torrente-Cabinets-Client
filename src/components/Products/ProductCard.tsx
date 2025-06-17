995b7aefcbd830bdb85eefa14f66c92e731d0b7a
stiratto <moranmoralesj4@gmail.com>
2025-06-17 12:55:59 -0500

    better design

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -4,6 +4,7 @@ import { IProduct } from "@/lib/interfaces/Product.Interface"
 import { addToCart } from "@/lib/utils"
 import { useNavigate } from "react-router-dom"
 import { Button } from "../ui/button"
+import { Check } from "lucide-react"
 
 export const ProductCard = ({ product, deleteProduct }: {
   product: IProduct,
@@ -21,23 +22,24 @@ export const ProductCard = ({ product, deleteProduct }: {
   return (
     <div
       onClick={handleClick}
-      className="cursor-pointer max-w-lg bg-white border border-gray-200 rounded-lg"
+      className="cursor-pointer max-w-lg bg-white border border-gray-200 shadow-sm rounded-lg"
     >
       <img
-        className="rounded-t-lg mx-auto aspect-square w-full"
+        className="rounded-t-lg mx-auto object-cover w-full h-[300px]"
         src={product.product_image}
         alt="Product image"
       />
-      <div className="p-8 dark:bg-gray-800 dark:border-gray-700">
-        <h5 className="text-xl text-start font-semibold tracking-tight text-gray-900 dark:text-white">
+      <div className="p-8">
+        <h3 className="text-2xl text-start uppercase font-extrabold">
           {product.product_name}
-        </h5>
-        <div className="grid grid-cols-1  justify-between items-center text-start gap-3">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">
-            ${product.product_price},00
-          </span>
+        </h3>
+        <div className="flex flex-col  justify-between text-start gap-3 ">
+          <p className="text-xl text-black ">
+            Starts at <span className="font-bold">${product.product_price},0</span>
+          </p>
+
           <Button
-            className="text-white hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 w-min"
+            className="rounded-none bg-transparent border-b border-[#FA1925] text-black hover:bg-none hover:bg-[#FA1925] hover:text-white uppercase font-medium text-sm px-5 py-2.5 w-min self-end"
             variant={"destructive"}
             onClick={(e) => {
               e.stopPropagation()
@@ -46,7 +48,12 @@ export const ProductCard = ({ product, deleteProduct }: {
           >
             Add to cart
           </Button>
-          {user?.role === "ADMIN" && (
+          <div className="flex flex-col text-sm gap-2">
+            <p className="flex items-center gap-2"><Check size={20} className="bg-[#fa1925] rounded-full p-1" /> In Stock & Ready to Ship</p>
+            <p className="flex items-center gap-2"><Check size={20} className="bg-[#FA1925] rounded-full p-1" /> Fast Shipping from U.S. Warehouse</p>
+          </div>
+          {/*
+         {user?.role === "ADMIN" && (
             <Button
               onClick={(e) => {
                 e.stopPropagation()
@@ -57,6 +64,7 @@ export const ProductCard = ({ product, deleteProduct }: {
               Delete Product
             </Button>
           )}
+           */}
         </div>
       </div>
     </div>
