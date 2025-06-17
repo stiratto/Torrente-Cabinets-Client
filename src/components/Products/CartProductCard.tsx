2d43f4558685157e91e600a09d9258239888eb38
stiratto <moranmoralesj4@gmail.com>
2025-06-17 12:55:28 -0500

    cartproduct card, better styling

--- src/components/Products/CartProductCard.tsx
+++ src/components/Products/CartProductCard.tsx
@@ -1,7 +1,8 @@
 import { CartProduct } from "@/lib/interfaces/Product.Interface"
-import { Minus, Plus, X } from "lucide-react"
+import { Minus, Plus, Trash } from "lucide-react"
 import { useCartContext } from "@/context/cartContext"
 import { Button } from "../ui/button"
+import { Separator } from "../ui/separator"
 
 
 
@@ -45,47 +46,50 @@ export const CartProductCard = ({ index, product }: { index: number, product: Ca
 
 
   return (
-    <li className="flex flex-col items-center justify-center">
+    <li className="flex flex-col items-start w-full">
       {product && (
-        <div className="flex flex-col items-left gap-3  py-3" key={index}>
-          <div className="relative">
+        <div className="flex flex-col md:flex-row items-start gap-4 py-3 w-full" key={index}>
+          <div className="relative flex flex-col w-full">
             <img
               src={product.product_image}
               alt="Product Image"
-              className="w-52 h-32 object-cover rounded-lg"
+              className="w-72 h-52 object-cover rounded self-center"
 
-            />  
-            <button onClick={removeFromCart} className="w-min absolute -top-1 -right-1 bg-red-500 rounded-sm text-white"><X size={15}/></button>
+            />
+
           </div>
 
-          <div className="flex flex-col gap-2">
-            <h1 className="text-xl font-bold">
+          <div className="flex flex-col gap-2 w-full justify-between h-full">
+            <h1 className="text-2xl font-bold flex items-center justify-between">
               {product.product_name}
+              <button onClick={removeFromCart} className="w-min rounded-sm text-black hover:bg-gray-300 p-1"><Trash size={20} /></button>
             </h1>
-            <p className="font-bold text-2xl">${product.product_price},00</p>
-            <div className="flex items-center gap-2">
-              <Button variant={"default"}
-                className="bg-white border border-gray-300 text-black hover:bg-gray-100"
-                disabled={product.product_quantity >= product.product_stock}
-                size={"sm"}
-                onClick={() => addOne(product.id as number)}
-              >
-                <Plus />
+            <div className="flex items-center justify-between gap-2">
+              <p className="text-xl font-medium"><span className="font-semibold">US${product.product_price},00</span> each</p>
+              <div className="flex items-center gap-2 border">
+                <Button
+                  className="bg-white border-none rounded-none text-black hover:bg-gray-100"
+                  disabled={product.product_quantity >= product.product_stock}
+                  size={"sm"}
+                  onClick={() => addOne(product.id as number)}
+                >
+                  <Plus />
+                </Button>
+                <span className="min-w-[3ch] text-center inline-block">{product.product_quantity}</span>
 
-              </Button>
-              {product.product_quantity}
-              <Button
-                variant={"default"}
-                className="bg-white border border-gray-300 text-black hover:bg-gray-100"
-                disabled={product.product_quantity === 1}
-                size={"sm"}
-                onClick={() => removeOne(product.id as number)}
-              ><Minus /></Button>
+
+                <Button
+                  className="bg-white border-none rounded-none text-black hover:bg-gray-100"
+                  disabled={product.product_quantity === 1}
+                  size={"sm"}
+                  onClick={() => removeOne(product.id as number)}
+                ><Minus /></Button>
+              </div>
             </div>
-
           </div>
         </div>
       )}
+      <Separator />
     </li>
   )
 }
